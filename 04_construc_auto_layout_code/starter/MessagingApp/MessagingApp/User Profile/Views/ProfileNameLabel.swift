72a8a30ae9ae27a95d9a7e0adbbbcb18093acd5b
Michael Rodriguez <miarodriguezur@unal.edu.co>
2021-11-23 17:04:31 -0500

    S2 C4 4.3 Building out a view controller's user interface in code. Refactoring profile name label

--- 04_construc_auto_layout_code/starter/MessagingApp/MessagingApp/User Profile/Views/ProfileNameLabel.swift
+++ 04_construc_auto_layout_code/starter/MessagingApp/MessagingApp/User Profile/Views/ProfileNameLabel.swift
@@ -28,24 +28,38 @@
 
 import UIKit
 
-@IBDesignable final class ProfileNameLabel: UILabel {
-  // MARK: - Properties
-  @IBInspectable private var isMultipleLine: Bool = false {
+final class ProfileNameLabel: UILabel {
+
+  // 1
+  override var text: String? {
     didSet {
-      isMultipleLineDidSet()
+      guard let words = text?
+        .components(separatedBy: .whitespaces)
+        else { return }
+      let joinedWords = words.joined(separator: "\n")
+      guard text != joinedWords else { return }
+      DispatchQueue.main.async { [weak self] in
+        self?.text = joinedWords
+      }
     }
   }
-  
-  // MARK: - Setup Views
-  private func isMultipleLineDidSet() {
-    switch isMultipleLine {
-    case true:
-      numberOfLines = 0
-      guard let words = text?.components(separatedBy: .whitespaces) else { return }
-      let joinedWords = words.joined(separator: "\n")
-      text = joinedWords
-    case false:
-      numberOfLines = 1
-    }
+
+  // 2
+  init(fullName: String? = "Full Name") {
+    super.init(frame: .zero)
+    setTextAttributes()
+    text = fullName
+  }
+
+  // 3
+  required init?(coder: NSCoder) {
+    super.init(coder: coder)
+  }
+
+  // 4
+  private func setTextAttributes() {
+    numberOfLines = 0
+    textAlignment = .center
+    font = UIFont.boldSystemFont(ofSize: 24)
   }
 }
