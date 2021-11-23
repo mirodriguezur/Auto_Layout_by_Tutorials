10faffed408cf8f05ca2ecef8f4701237127c670
Michael Rodriguez <miarodriguezur@unal.edu.co>
2021-11-23 17:26:36 -0500

    S2 C4 4.3 Building out a view controller's user interface in code. Refactoring stack views.

--- 04_construc_auto_layout_code/starter/MessagingApp/MessagingApp/User Profile/Views/ProfileHeaderView.swift
+++ 04_construc_auto_layout_code/starter/MessagingApp/MessagingApp/User Profile/Views/ProfileHeaderView.swift
@@ -32,10 +32,116 @@ final class ProfileHeaderView: UIView {
   override init(frame: CGRect) {      //Anulo metodo de inicialización
     super.init(frame: frame)          //establece color de fondo.
     backgroundColor = .groupTableViewBackground
+    setupStackView()
+
   }
   
   //cuando se crea una vista en código, init(frame:)se usa el inicializador y cuando se crea una vista a partir de un guión gráfico o .xib, init(coder:)se usa en su lugar
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
+  }
+  
+  // 1
+  private let profileImageView =
+    ProfileImageView(borderShape: .squircle)
+  private let leftSpacerView = UIView()
+  private let rightSpacerView = UIView()
+
+  private let fullNameLabel = ProfileNameLabel()
+
+  // 2
+  private let messageButton =
+    UIButton.createSystemButton(withTitle: "Message")
+  private let callButton =
+    UIButton.createSystemButton(withTitle: "Call")
+  private let emailButton =
+    UIButton.createSystemButton(withTitle: "Email")
+
+  // 3
+  private lazy var profileImageStackView =
+    UIStackView(arrangedSubviews:
+      [leftSpacerView, profileImageView, rightSpacerView])
+
+  private lazy var profileStackView: UIStackView = {
+    let stackView = UIStackView(arrangedSubviews:
+      [profileImageStackView, fullNameLabel])
+    stackView.distribution = .fill
+    stackView.axis = .vertical
+    stackView.spacing = 16
+    return stackView
+  }()
+
+  private lazy var actionStackView: UIStackView = {
+    let stackView = UIStackView(arrangedSubviews:
+      [messageButton, callButton, emailButton])
+    stackView.distribution = .fillEqually
+    return stackView
+  }()
+
+  private lazy var stackView: UIStackView = {
+    let stackView = UIStackView(arrangedSubviews:
+      [profileStackView, actionStackView])
+    stackView.axis = .vertical
+    stackView.spacing = 16
+    return stackView
+  }()
+  
+  private func setupStackView() {
+    // 1
+    addSubview(stackView)
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    
+    // 2
+    NSLayoutConstraint.activate(
+      [stackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+       stackView.leadingAnchor.constraint(
+        greaterThanOrEqualTo: leadingAnchor, constant: 20),
+       stackView.leadingAnchor.constraint(
+        lessThanOrEqualTo: leadingAnchor, constant: 500),
+       stackView.bottomAnchor.constraint(
+        equalTo: bottomAnchor, constant: -8),
+       stackView.topAnchor.constraint(
+        equalTo: topAnchor, constant: 26),
+       
+       profileImageView.widthAnchor.constraint(
+        equalToConstant: 120),
+       profileImageView.widthAnchor.constraint(
+        equalTo: profileImageView.heightAnchor),
+       
+       leftSpacerView.widthAnchor.constraint(
+        equalTo: rightSpacerView.widthAnchor)
+      ])
+    
+    // 3
+    profileImageView.setContentHuggingPriority(
+      UILayoutPriority(251),
+      for: NSLayoutConstraint.Axis.horizontal)
+    profileImageView.setContentHuggingPriority(
+      UILayoutPriority(251),
+      for: NSLayoutConstraint.Axis.vertical)
+    
+    fullNameLabel.setContentHuggingPriority(
+      UILayoutPriority(251),
+      for: NSLayoutConstraint.Axis.horizontal)
+    fullNameLabel.setContentHuggingPriority(
+      UILayoutPriority(251),
+      for: NSLayoutConstraint.Axis.vertical)
+    fullNameLabel.setContentCompressionResistancePriority(
+      UILayoutPriority(751),
+      for: NSLayoutConstraint.Axis.vertical)
+    
+    messageButton.setContentCompressionResistancePriority(
+      UILayoutPriority(751),
+      for: NSLayoutConstraint.Axis.horizontal)
+  }
+
+}
+
+private extension UIButton {
+  static func createSystemButton(withTitle title: String)
+    -> UIButton {
+      let button = UIButton(type: .system)
+      button.setTitle(title, for: .normal)
+      return button
   }
 }
